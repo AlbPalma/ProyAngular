e36961fd57d3a1bda88e20c0258f5c80cc51c8cf
AlbPalma <palmaa232@gmail.com>
2024-05-27 23:30:48 +0200

    Descripción de los cambios antes de la actualización de Angular

---
+++ src/app/perfil-a/perfil-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-a.component.html',
+  styleUrl: './perfil-a.component.css'
+})
+export class PerfilAComponent {
+
+}
