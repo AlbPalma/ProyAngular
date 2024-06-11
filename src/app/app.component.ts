d2367e5f869181e6ba1263620c7f464d68c6e974
AlbPalma <palmaa232@gmail.com>
2024-06-11 18:59:57 +0200

    Versión con todos los menús y navegación.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterOutlet } from '@angular/router';
 import { PopupComponent } from './popup/popup.component';
-
+import { SubirContenidoComponent } from './subir-contenido/subir-contenido.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,6 +15,10 @@ export class AppComponent {
   constructor(private dialogRef : MatDialog){}
   openDialog(){
     this.dialogRef.open(PopupComponent,{height:"80vh",width:"80vw"});
+  }
+
+  openSubir(){
+    this.dialogRef.open(SubirContenidoComponent,{height:"25vh",width:"10vw"});
   }
 
 }
